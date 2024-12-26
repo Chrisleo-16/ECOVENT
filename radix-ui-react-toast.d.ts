668cf3fb93161f97f1b35a32ab1b938f1a87ddec
Chrisleo-16 <152534621+Chrisleo-16@users.noreply.github.com>
2024-12-26 13:04:33 +0300

    Create radix-ui-react-toast.d.ts

---
+++ radix-ui-react-toast.d.ts
@@ -0,0 +1,9 @@
+declare module '@radix-ui/react-toast' {
+    export const ToastProvider: React.FC;
+    export const Toast: React.FC;
+    export const ToastTitle: React.FC;
+    export const ToastDescription: React.FC;
+    export const ToastAction: React.FC;
+    export const ToastViewport: React.FC;
+    // Add other exports as needed
+  }
